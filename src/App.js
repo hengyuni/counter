78e8de5717679970efb5c834ebfb2336ef9ea34a
hengyuni <hengyuni@gmail.com>
2021-07-23 16:25:53 -0400

    random numbers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const [dice, setDice] = useState(1);
 
 	// function to decrement count
 	const decrementCount = () => {
@@ -18,12 +19,21 @@ function App() {
 		}
 	};
 
+	// function that spits random number
+	const roll = () => {
+		setDice(Math.ceil(Math.random() * 5 + 1));
+	};
+
 	return (
 		<div className="App">
 			<h1>Counter Punch</h1>
 			<button onClick={decrementCount}>-</button>
 			<span>{count}</span>
 			<button onClick={incrementCount}>+</button>
+
+			<h1>Dice Game</h1>
+			<span>{dice}</span>
+			<button onClick={roll}>Roll</button>
 		</div>
 	);
 }
